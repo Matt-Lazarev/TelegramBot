c052509d0e9b1cb5199ada62c89886228e12e8d2
Matt Lazarev <matv26-01@mail.ru>
2021-05-20 01:23:26 +0300

    Fixes

---
+++ src/main/java/PingTask.java
@@ -0,0 +1,31 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PingTask {
+    @Value("${pingtask.url}")
+    private String url;
+
+    @Scheduled(fixedRateString = "${pingtask.period}")
+    public void pingMe() {
+        try {
+            URL url = new URL(getUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
